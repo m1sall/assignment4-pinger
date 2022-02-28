e981d7a158f4a8ae82802c1b5243f15708cd854c
fasima1 <ms13592@nyu.edu>
2022-02-28 01:38:33 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -11,22 +11,18 @@ import binascii
 
 ICMP_ECHO_REQUEST = 8
 
-
 def checksum(string):
     csum = 0
     countTo = (len(string) // 2) * 2
     count = 0
-
     while count < countTo:
         thisVal = (string[count + 1]) * 256 + (string[count])
         csum = csum + thisVal
         csum = csum & 0xffffffff
         count = count + 2
-
     if countTo < len(string):
         csum = csum + (string[len(string) - 1])
         csum = csum & 0xffffffff
-
     csum = (csum >> 16) + (csum & 0xffff)
     csum = csum + (csum >> 16)
     answer = ~csum
@@ -34,10 +30,7 @@ def checksum(string):
     answer = answer >> 8 | (answer << 8 & 0xff00)
     return answer
 
-
-
 def receiveOnePing(mySocket, ID, timeout, destAddr):
-    global packageRev,timeRTT
     timeLeft = timeout
     while 1:
         startedSelect = time.time()
@@ -66,7 +59,6 @@ def receiveOnePing(mySocket, ID, timeout, destAddr):
 
 def sendOnePing(mySocket, destAddr, ID):
     # Header is type (8), code (8), checksum (16), id (16), sequence (16)
-
     myChecksum = 0
     # Make a dummy header with a 0 checksum
     # struct -- Interpret strings as packed binary data
@@ -74,26 +66,20 @@ def sendOnePing(mySocket, destAddr, ID):
     data = struct.pack("d", time.time())
     # Calculate the checksum on the data and the dummy header.
     myChecksum = checksum(header + data)
-
     # Get the right checksum, and put in the header
-
     if sys.platform == 'darwin':
             # Convert 16-bit integers from host to network  byte order
             myChecksum = htons(socket(myChecksum) & 0xffff)
     else:
             myChecksum = htons(myChecksum)
-
     header = struct.pack("bbHHh", ICMP_ECHO_REQUEST, 0, myChecksum, ID, 1)
     packet = header + data
-
     mySocket.sendto(packet, (destAddr, 1))  # AF_INET address must be tuple, not str
-
     # Both LISTS and TUPLES consist of a number of objects
     # which can be referenced by their position number within the object.
-
+    
 def doOnePing(destAddr, timeout):
     icmp = getprotobyname("icmp")
-
     # SOCK_RAW is a powerful socket type. For more details:   http://sockraw.org/papers/sock_raw
     mySocket = socket(AF_INET, SOCK_RAW, icmp)
     myID = os.getpid() & 0xFFFF  # Return the current process i
