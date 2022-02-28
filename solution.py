e503956885a1f804875aa83f6abec07ab0911545
fasima1 <ms13592@nyu.edu>
2022-02-28 02:14:47 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,4 +1,5 @@
 from audioop import avg
+from distutils.filelist import glob_to_re
 from socket import *
 import os
 from statistics import stdev
@@ -10,6 +11,7 @@ import binascii
 # Should use stdev
 
 ICMP_ECHO_REQUEST = 8
+
 
 def checksum(string):
     csum = 0
@@ -30,34 +32,37 @@ def checksum(string):
     answer = answer >> 8 | (answer << 8 & 0xff00)
     return answer
 
+
+
 def receiveOnePing(mySocket, ID, timeout, destAddr):
+    global packageRev, timeRTT
     timeLeft = timeout
     while 1:
-        startedSelect = time.time()
-        whatReady = select.select([mySocket], [], [], timeLeft)
-        howLongInSelect = (time.time() - startedSelect)
-        if whatReady[0] == []: # Timeout
-            return "0: Destination Network Unreachable,"
-        timeReceived = time.time()
-        recPacket, addr = mySocket.recvfrom(1024)
-        #Fill in start
-        #Fetch the ICMP header from the IP packet
-        icmpHeader = recPacket[20:28]
-        requestType, code, revChecksum, revId, revSequence = struct.unpack('bbHHh',icmpHeader)
-        if ID == revId:
-            bytesInDouble = struct.calcsize('d')
-            timeData = struct.unpack('d',recPacket[28:28 + bytesInDouble])[0]
-            timeRTT= (timeReceived - timeData)
-            packageRev = 1
-            return timeReceived - timeData
-        else:
-            return "ID is not the same!"
-        #Fill in end
-        timeLeft = timeLeft - howLongInSelect
-        if timeLeft <= 0:
-            return "1: Destination Host Unreachable."
+     startedSelect = time.time()
+    whatReady = select.select([mySocket], [], [], timeLeft)
+    howLongInSelect = (time.time() - startedSelect)
+    if whatReady[0] == []: # Timeout
+     return "Request timed out."
+    timeReceived = time.time()
+    recPacket, addr = mySocket.recvfrom(1024)
+    #Fill in start
+    #Fetch the ICMP header from the IP packet
+    icmph = recPacket[20:28]
+    type, code, checksum, pID, sq = struct.unpack("bbHHh", icmph)  
+    print("The header received in the ICMP reply is ",type, code, checksum, pID, sq)
+    if pID == ID:
+       bytesinDbl = struct.calcsize("d")
+    timeSent = struct.unpack("d", recPacket[28:28 + bytesinDbl])[0]
+    rtt = timeReceived - timeSent
+    print ("RTT is : ")
+    return (rtt)   
+    # Fill in end
+    timeLeft = timeLeft - howLongInSelect
+    if timeLeft <= 0:
+       print ("Request timed out.")
 
 def sendOnePing(mySocket, destAddr, ID):
+    global packageSent
     # Header is type (8), code (8), checksum (16), id (16), sequence (16)
     myChecksum = 0
     # Make a dummy header with a 0 checksum
@@ -71,13 +76,13 @@ def sendOnePing(mySocket, destAddr, ID):
             # Convert 16-bit integers from host to network  byte order
             myChecksum = htons(socket(myChecksum) & 0xffff)
     else:
-            myChecksum = htons(myChecksum)
+        myChecksum = htons(myChecksum)
     header = struct.pack("bbHHh", ICMP_ECHO_REQUEST, 0, myChecksum, ID, 1)
     packet = header + data
     mySocket.sendto(packet, (destAddr, 1))  # AF_INET address must be tuple, not str
     # Both LISTS and TUPLES consist of a number of objects
     # which can be referenced by their position number within the object.
-    
+
 def doOnePing(destAddr, timeout):
     icmp = getprotobyname("icmp")
     # SOCK_RAW is a powerful socket type. For more details:   http://sockraw.org/papers/sock_raw
