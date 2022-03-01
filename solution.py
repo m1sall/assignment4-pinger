5d5b71dacf97f527bfd28155a26d537ad4b4410b
fasima1 <ms13592@nyu.edu>
2022-02-28 19:29:01 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -11,6 +11,7 @@ import binascii
 
 ICMP_ECHO_REQUEST = 8
 
+
 def checksum(string):
     csum = 0
     countTo = (len(string) // 2) * 2
@@ -18,12 +19,12 @@ def checksum(string):
 
     while count < countTo:
         thisVal = (string[count + 1]) * 256 + (string[count])
-    csum = csum + thisVal
-    csum = csum & 0xffffffff
-    count = count + 2
+        csum = csum + thisVal
+        csum = csum & 0xffffffff
+        count = count + 2
 
     if countTo < len(string):
-        csum = csum + ord(string[len(string) - 1])
+        csum = csum + (string[len(string) - 1])
         csum = csum & 0xffffffff
 
     csum = (csum >> 16) + (csum & 0xffff)
@@ -33,36 +34,43 @@ def checksum(string):
     answer = answer >> 8 | (answer << 8 & 0xff00)
     return answer
 
+
+
 def receiveOnePing(mySocket, ID, timeout, destAddr):
-    global packageRev,timeRTT
-    timeLeft = timeout
-    while 1:
-     startedSelect = time.time()
-    whatReady = select.select([mySocket], [], [], timeLeft)
-    howLongInSelect = (time.time() - startedSelect)
-    if whatReady[0] == []:
-    #Time out    
-        return "Request timed out."
-    timeReceived = time.time()
-    recPacket, addr = mySocket.recvfrom(1024)
-    #Fill in start
-    #Fetch the ICMP header from the IP packet
-    icmph = recPacket[20:28]
-    type, code, checksum, pID, sq = struct.unpack("bbHHh", icmph)  
-    print("The header received in the ICMP reply is ",type, code, checksum, pID, sq)
-    if pID == ID:
-     bytesinDbl = struct.calcsize("d")
-    timeSent = struct.unpack("d", recPacket[28:28 + bytesinDbl])[0]
-    rtt = timeReceived - timeSent
-    print ("RTT is : ")
-    return rtt  
-    # Fill in end
-    timeLeft = timeLeft - howLongInSelect
-    if timeLeft <= 0:
-        print ("Request timed out.")
+
+   timeLeft = timeout
+
+   while 1:
+
+    startedSelect = time.time()
+   whatReady = select.select([mySocket], [], [], timeLeft)
+   howLongInSelect = (time.time() - startedSelect)
+   if whatReady[0] == []: # Timeout
+    return "Request timed out."
+
+   timeReceived = time.time()
+   recPacket, addr = mySocket.recvfrom(1024)
+
+  #Fill in start
+  #Fetch the ICMP header from the IP packet
+   icmph = recPacket[20:28]
+   type, code, checksum, pID, sq = struct.unpack("bbHHh", icmph)
+  
+   print("The header received in the ICMP reply is ",type, code, checksum, pID, sq)
+   if pID == ID:
+    bytesinDbl = struct.calcsize("d")
+   timeSent = struct.unpack("d", recPacket[28:28 + bytesinDbl])[0]
+   rtt = timeReceived - timeSent
+   print ("RTT is : ")
+   return (rtt)   
+  # Fill in end
+   timeLeft = timeLeft - howLongInSelect
+   if timeLeft <= 0:
+    print ("Request timed out.")
 
 def sendOnePing(mySocket, destAddr, ID):
     # Header is type (8), code (8), checksum (16), id (16), sequence (16)
+
     myChecksum = 0
     # Make a dummy header with a 0 checksum
     # struct -- Interpret strings as packed binary data
@@ -70,19 +78,26 @@ def sendOnePing(mySocket, destAddr, ID):
     data = struct.pack("d", time.time())
     # Calculate the checksum on the data and the dummy header.
     myChecksum = checksum(header + data)
+
     # Get the right checksum, and put in the header
+
     if sys.platform == 'darwin':
-    # Convert 16-bit integers from host to network  byte order
+            # Convert 16-bit integers from host to network  byte order
             myChecksum = htons(socket(myChecksum) & 0xffff)
     else:
             myChecksum = htons(myChecksum)
+
     header = struct.pack("bbHHh", ICMP_ECHO_REQUEST, 0, myChecksum, ID, 1)
     packet = header + data
+
     mySocket.sendto(packet, (destAddr, 1))  # AF_INET address must be tuple, not str
+
     # Both LISTS and TUPLES consist of a number of objects
     # which can be referenced by their position number within the object.
+
 def doOnePing(destAddr, timeout):
     icmp = getprotobyname("icmp")
+
     # SOCK_RAW is a powerful socket type. For more details:   http://sockraw.org/papers/sock_raw
     mySocket = socket(AF_INET, SOCK_RAW, icmp)
     myID = os.getpid() & 0xFFFF  # Return the current process i
