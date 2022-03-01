8a611169de0261f25edd97c843e3229e327a78a0
fasima1 <ms13592@nyu.edu>
2022-02-28 21:25:26 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -114,7 +114,7 @@ def ping(host, timeout=1):
         time.sleep(1)# one second
     packet_min = min(list)*1000
     packet_max = max(list)*1000
-    packet_avg = avg(list)*1000
+    packet_avg = statistics.mean(list)*1000
     stdev_var = (list)*1000
     vars = ([str(round(packet_min, 2)), str(round(packet_avg, 2)), str(round(packet_max, 2)),str(round(statistics.stdev(stdev_var), 2))])
 
